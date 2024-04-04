457af209a65a2859975f976a34c8faf81d16b093
Evelyn Chen <96403274+lindcrj@users.noreply.github.com>
2024-04-03 20:21:38 -0500

    Update 584_Find_Customer_Referee.sql
    Runtime 1120 ms
    Beats 23.81% of users with MySQL

--- Basics/584_Find_Customer_Referee.sql
+++ Basics/584_Find_Customer_Referee.sql
@@ -1,18 +1,3 @@
--- Solution 1: Basics
-SELECT name 
-FROM customer
-WHERE ISNULL(referee_id,0) != 2;
-
-
-
--- Solution 2: Basics
-SELECT name 
-FROM customer
-WHERE COALESCE(referee_id,0) != 2;
-
-
-
--- Solution 3: Basics
 SELECT name
-FROM customer
-WHERE referee_id != 2 OR referee_id IS NULL;
+FROM Customer
+WHERE referee_id != 2 or referee_id IS NULL;
