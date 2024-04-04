bd47db92169bee29134ec52562b7242f2bea2c86
Evelyn Chen <96403274+lindcrj@users.noreply.github.com>
2024-04-04 13:30:09 -0500

    Update 181_Employees_Earning_More_Than_Their_Managers.sql
    772 ms
    Beats 47.72% of users with MySQL

--- Join/Simple-Join/181_Employees_Earning_More_Than_Their_Managers.sql
+++ Join/Simple-Join/181_Employees_Earning_More_Than_Their_Managers.sql
@@ -1,5 +1,5 @@
-SELECT e1.name AS Employee
-FROM employee e1
-JOIN employee e2
-    ON e1.Managerid = e2.id
-WHERE e1.salary > e2.salary;
+SELECT e1.name as Employee
+FROM Employee e1 
+JOIN Employee e2 
+ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary
