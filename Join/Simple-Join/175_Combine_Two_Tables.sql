dd45ce0c7a0d074787d5a5dcbf1bc8190f44cb8f
Evelyn Chen <96403274+lindcrj@users.noreply.github.com>
2024-04-04 13:29:21 -0500

    Update 175_Combine_Two_Tables.sql
    1296 ms
    Beats 5.29% of users with MySQL

--- Join/Simple-Join/175_Combine_Two_Tables.sql
+++ Join/Simple-Join/175_Combine_Two_Tables.sql
@@ -1,5 +1,4 @@
-SELECT p.firstname, p.lastname, a.city, a.state
-FROM person p
--- LEFT JOIN return all records from the left table, and the matched records from the right table
-LEFT JOIN address a
-  ON p.personid = a.personid;
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId;
