a03100cc13e50c79fd51344dc4fe37232d8e8563
Evelyn Chen <96403274+lindcrj@users.noreply.github.com>
2024-04-04 12:48:46 -0500

    Update 1141_User_Activity_for_the_Past_30_Days_I.sql
    1672 ms
    Beats 5.00% of users with MySQL

--- Basics/1141_User_Activity_for_the_Past_30_Days_I.sql
+++ Basics/1141_User_Activity_for_the_Past_30_Days_I.sql
@@ -1,4 +1,11 @@
-SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+# Solution 1
+SELECT activity_date as day, COUNT(distinct user_id) AS active_users
+FROM Activity
+WHERE datediff('2019-07-27', activity_date) < 30 AND activity_date <= '2019-07-27'
+GROUP BY activity_date;
+
+# Solution 2
+SELECT activity_date as day, COUNT(distinct user_id) AS active_users
 FROM Activity
 GROUP BY activity_date
-HAVING activity_date BETWEEN  DATEADD(day,-29,'2019-07-27') and '2019-07-27';
+HAVING activity_date between '2019-06-28' and '2019-07-27';
