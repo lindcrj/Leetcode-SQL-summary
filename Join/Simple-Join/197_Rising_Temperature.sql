2463d7cb21019aa33b469ac53acc243def2f37ef
Evelyn Chen <96403274+lindcrj@users.noreply.github.com>
2024-04-05 14:19:24 -0500

    Update 197_Rising_Temperature.sql
    1162 ms
    Beats 17.86% of users with MySQL

--- Join/Simple-Join/197_Rising_Temperature.sql
+++ Join/Simple-Join/197_Rising_Temperature.sql
@@ -1,5 +1,5 @@
-SELECT w2.id
-FROM weather w1
-JOIN weather w2
-    ON DATEADD(day,1,w1.recorddate) = w2.recorddate
-WHERE w1.temperature < w2.temperature;
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 
+AND w1.temperature > w2.temperature;
+
